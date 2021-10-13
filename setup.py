8f7d3bce673ac6b62156d6bd9681a95637a457b0
Bill Bradley <billbradley@mirabolic.net>
2021-10-12 22:20:03 -0400

    Updating all the version tags (3 places!)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name='mirabolic',
     packages=find_namespace_packages(),
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     description='Statistical and Machine Learning tools from Mirabolic',
     long_description=README,
